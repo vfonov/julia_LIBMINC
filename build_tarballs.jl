8651c67d24721fcbe9d5fb2df7733e664a571ab0
Vladimir S. FONOV <vladimir.fonov@gmail.com>
2023-11-14 14:35:35 -0500

    Updating build...

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -1,12 +1,12 @@
 using BinaryBuilder, Pkg
 
 name = "LIBMINC"
-version = v"2.4.12"
+version = v"2.4.13"
+
 sources = [
-    #ArchiveSource("https://github.com/BIC-MNI/libminc/archive/refs/heads/develop.tar.gz", "cad1b1797b83906f5a042648d20615bd077b01d3e6c4ed885ea37fe7e9a9c8b9"),
-    #DirectorySource("/home/vfonov/src/libminc")
-    GitSource("git@github.com:BIC-MNI/libminc.git","aa08255f0856e70fb001c5f9ee1f4e5a8c12d47d")
-    GitSource("git@github.com:NIST-MNI/minc2-simple.git","e2d2d14ba15aff7dbcadeb3145f17d88bb4f3b72")
+    #ArchiveSource("https://github.com/BIC-MNI/libminc/archive/refs/tags/release-2.4.06.tar.gz", "cd5c6da9cd98be225a4bd3b8d712bd5292fc24f434cae732fa37af866c2db5b3"),
+    GitSource("git@github.com:BIC-MNI/libminc.git","15d994def83274b715711afffa73268167ec2cc6") # release-2.4.06
+    GitSource("git@github.com:NIST-MNI/minc2-simple.git","e7203f04941faf45cd7cea9149e7b196672fb117")
 ]
 
 script = raw"""
@@ -19,7 +19,7 @@ cmake .. \
     -DCMAKE_BUILD_TYPE=Release \
     -DLIBMINC_BUILD_SHARED_LIBS:BOOL=ON \
     -DLIBMINC_MINC1_SUPPORT:BOOL=ON \
-    -DBUILD_TESTING:BOOL=OFF \
+    -DBUILD_TESTING:BOOL=ON \
     -DLIBMINC_USE_NIFTI:BOOL=OFF \
     -DLIBMINC_BUILD_EZMINC=OFF
 make -j${nproc}
@@ -41,13 +41,12 @@ make install
     
 
 install_license ${WORKSPACE}/srcdir/libminc/COPYING
-
 """
 
 #platforms = supported_platforms()
-
+# right now only linux on x86_64 is tested
 platforms = [
-    Platform("x86_64", "Linux";libc="glibc"),
+    Platform("x86_64", "Linux"; libc="glibc"),
 ]
 
 #platforms = expand_cxxstring_abis(platforms)
@@ -58,10 +57,9 @@ products = [
 ]
 
 dependencies = [
-    Dependency(PackageSpec(;name="HDF5_jll", uuid="0234f1f7-429e-5d53-9886-15a909be8d59"),v"1.12.2+2",compat="~1.12.2"),
-    Dependency(PackageSpec(;name="NetCDF_jll",uuid="7243133f-43d8-5620-bbf4-c2c921802cf3"),v"400.902.5+1",compat="~400.902.5"), # 
-    # Dependency("HDF5_jll"),
-    # Dependency("NetCDF_jll"), # 
+   Dependency("HDF5_jll";  compat="~1.12.2"), # should be compatible with NetCDF
+   Dependency("NetCDF_jll";compat="~400.902.5"), 
 ]
 
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; 
+    julia_compat="1.6", preferred_gcc_version=v"5")
